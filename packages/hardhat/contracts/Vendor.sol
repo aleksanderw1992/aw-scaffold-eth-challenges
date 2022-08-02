34b3b57dbfb9ca9b5439ab968d2f0f9d03f899e7
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-08-03 00:26:08 +0200

    In step 3 - allow buying tokens

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  uint256 public constant tokensPerEth = 100;
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
@@ -14,10 +14,20 @@ contract Vendor is Ownable {
     yourToken = YourToken(tokenAddress);
   }
 
-  // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 noTokens = msg.value * tokensPerEth;
+        bool ok = yourToken.transfer(msg.sender, noTokens);
+        if(!ok) {
+            revert("transfer() failed");
+        }
+        emit BuyTokens(msg.sender, msg.value, noTokens);
+    }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
   // ToDo: create a sellTokens(uint256 _amount) function:
 
+    fallback() external payable {
+
+    }
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YourToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint( address(0x2117Dfd0E98fD6ec0e43c0e76F458D78aa22c664) , 1000 * 10 ** 18);
     }
 }
