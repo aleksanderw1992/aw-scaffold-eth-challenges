05e514919c7716c2f6ce6e028ad56ead2cebb3d7
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-08-14 19:28:55 +0200

    Checkpoint 2: Reserves - verify Under the debug tab, my DEX shows 5 ETH and 5 Balloons of liquidity

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -6,10 +6,21 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract DEX {
 
   IERC20 token;
+    uint256 public totalLiquidity;
+    mapping (address => uint256) public liquidity;
 
   constructor(address token_addr) {
     token = IERC20(token_addr);
   }
+    
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DEX: init - already has liquidity");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(token.transferFrom(msg.sender, address(this), tokens), "DEX: init - transfer did not transact");
+        return totalLiquidity;
+    }
+
 
   // write your functions here...
 
