0591ce73fdb035a223edc3b3bf15e1000e3646e2
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-08-14 19:57:57 +0200

    Checkpoint 4: Trading - add methods to swap

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -2,12 +2,14 @@ pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract DEX {
-
+  
+  using SafeMath for uint256;
   IERC20 token;
-    uint256 public totalLiquidity;
-    mapping (address => uint256) public liquidity;
+  uint256 public totalLiquidity;
+  mapping (address => uint256) public liquidity;
 
   constructor(address token_addr) {
     token = IERC20(token_addr);
@@ -23,5 +25,45 @@ contract DEX {
 
 
   // write your functions here...
+    
 
+    function price(
+        uint256 xInput,
+        uint256 xReserves,
+        uint256 yReserves
+    ) public view returns (uint256 yOutput) {
+        uint256 xInputWithFee = xInput.mul(997);
+        uint256 numerator = xInputWithFee.mul(yReserves);
+        uint256 denominator = (xReserves.mul(1000)).add(xInputWithFee);
+        return (numerator / denominator);
+    }
+    
+    /**
+     * @notice sends Ether to DEX in exchange for $BAL
+     */
+    function ethToToken() public payable returns (uint256 tokenOutput) {
+        require(msg.value > 0, "cannot swap 0 ETH");
+        uint256 ethReserve = address(this).balance.sub(msg.value);
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 tokenOutput = price(msg.value, ethReserve, token_reserve);
+        
+        require(token.transfer(msg.sender, tokenOutput), "ethToToken(): reverted swap.");
+        emit EthToTokenSwap(msg.sender, "Eth to Balloons", msg.value, tokenOutput);
+        return tokenOutput;
+    }
+    
+    /**
+     * @notice sends $BAL tokens to DEX in exchange for Ether
+     */
+    function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {
+        require(tokenInput > 0, "cannot swap 0 tokens");
+        uint256 token_reserve = token.balanceOf(address(this));
+        uint256 ethOutput = price(tokenInput, token_reserve, address(this).balance);
+        require(token.transferFrom(msg.sender, address(this), tokenInput), "tokenToEth(): reverted swap.");
+        (bool sent, ) = msg.sender.call{ value: ethOutput }("");
+        require(sent, "tokenToEth: revert in transferring eth to you!");
+        emit TokenToEthSwap(msg.sender, "Balloons to ETH", ethOutput, tokenInput);
+        return ethOutput;
+    }
+    
 }
